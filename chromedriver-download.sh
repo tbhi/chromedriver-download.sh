e5b9e3effa2a86edb77a21d995b26b2b89047757
Tristan Hill <t23+git@eaux.uk>
2023-07-19 10:21:29 +0100

    api changed for finding download

--- chromedriver-download.sh
+++ chromedriver-download.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 CHROME_VERSION=$(google-chrome --version | sed -En 's/.* ([0-9]+).*/\1/p')
 DRIVER_VERSION=$(chromedriver --version | sed -En 's/.* ([0-9.]+).*/\1/p')
-LATEST_DRIVER_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+JSON="$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json)"
+LATEST_DRIVER_VERSION="$(echo "$JSON" | jq -r .channels.Stable.version)"
 [ "$DRIVER_VERSION" = "$LATEST_DRIVER_VERSION" ] && exit 0
 DIR="$(mktemp -d)"
-curl -so "$DIR"/chromedriver.zip https://chromedriver.storage.googleapis.com/$LATEST_DRIVER_VERSION/chromedriver_linux64.zip
+curl -so "$DIR"/chromedriver.zip "$(echo "$JSON" | jq -r .channels.Stable.downloads.chromedriver[0].url)"
 unzip "$DIR"/chromedriver.zip -d "$DIR"/out
-INSTALL_DIR="$(dirname "$(which chromedriver || echo $0)")"
+INSTALL_DIR="$(dirname "$(which chromedriver 2>/dev/null || echo $0)")"
 find "$DIR/out" -type f -perm /u+x -exec mv {} "$INSTALL_DIR" \;
 echo installed version "$LATEST_DRIVER_VERSION"
 rm -rf "$DIR"
